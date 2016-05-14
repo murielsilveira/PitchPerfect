9388faca3955049ebb50fbc52553edc0664d8fd3
Muriel Silveira <murielkong@gmail.com>
2016-05-14 00:55:47 -0300

    Create record and stop recording buttons and a label for instructions

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var instructionLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func startRecording(sender: AnyObject) {
+        instructionLabel.text = "Recording..."
+    }
 
+    @IBAction func stopRecording(sender: AnyObject) {
+        instructionLabel.text = "Tap to Record"
+    }
 }
-
