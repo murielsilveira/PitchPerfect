4a1e172df75e83ea17bde852be4fafadb9c0430d
Muriel Silveira <murielkong@gmail.com>
2016-05-15 18:21:09 -0300

    Create IBOutlets and IBActions to all buttons and add a Audio class extension to help dealing with audio manipulation

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -7,16 +7,52 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
+    
+    @IBOutlet weak var snailButton: UIButton!
+    @IBOutlet weak var rabbitButton: UIButton!
+    @IBOutlet weak var chipmunkButton: UIButton!
+    @IBOutlet weak var darthVaderButton: UIButton!
+    @IBOutlet weak var echoButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
 
     var recordedAudioURL: NSURL!
+    var audioFile: AVAudioFile!
+    var audioEngine: AVAudioEngine!
+    var audioPlayerNode: AVAudioPlayerNode!
+    var stopTimer: NSTimer!
+    
+    enum ButtonType: Int {
+        case Slow = 0,
+        Fast,
+        Chipmunk,
+        DarthVader,
+        Echo,
+        Reverb
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupAudio()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        configureUI(.NotPlaying)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-    }    
+    }
+    
+    @IBAction func playSoundForButton(sender: UIButton) {
+        print("Play pressed")
+    }
+    
+    @IBAction func stopButtonPressed(sender: AnyObject) {
+        print("Stop pressed")
+    }
 }
