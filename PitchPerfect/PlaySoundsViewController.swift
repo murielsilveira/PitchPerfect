cc71548828d522e6f618fe5a0bdbae86baa835e2
Muriel Silveira <murielkong@gmail.com>
2016-05-15 15:15:12 -0300

    Add a segue to the recordVC and pass the recordedAudioURL to the playSoundsVC

---
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  PlaySoundsViewController.swift
+//  PitchPerfect
+//
+//  Created by Muriel Silveira Pereira on 5/15/16.
+//  Copyright © 2016 Muriel Silveira Pereira. All rights reserved.
+//
+
+import UIKit
+
+class PlaySoundsViewController: UIViewController {
+
+    var recordedAudioURL: NSURL!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }    
+}
---
+++ PitchPerfect/RecordViewController.swift
@@ -0,0 +1,78 @@
+//
+//  RecordViewController.swift
+//  PitchPerfect
+//
+//  Created by Muriel Silveira Pereira on 5/13/16.
+//  Copyright © 2016 Muriel Silveira Pereira. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class RecordViewController: UIViewController, AVAudioRecorderDelegate {
+
+    @IBOutlet weak var instructionLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
+    var audioRecorder:AVAudioRecorder!
+    let stopRecordingSegue = "stopRecordingSegue"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        stopRecordingButton.enabled = false
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func startRecording(sender: AnyObject) {
+        instructionLabel.text = "Recording..."
+        recordButton.enabled = false
+        stopRecordingButton.enabled = true
+        
+        let dirPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String
+        
+        let fileName = "recordedVoide.wav"
+        let pathArray = [dirPath, fileName]
+        let filePath = NSURL.fileURLWithPathComponents(pathArray)
+        print(filePath)
+        
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+        
+        try! audioRecorder = AVAudioRecorder(URL: filePath!, settings: [:])
+        audioRecorder.delegate = self
+        audioRecorder.meteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
+    }
+
+    @IBAction func stopRecording(sender: AnyObject) {
+        instructionLabel.text = "Tap to Record"
+        recordButton.enabled = true
+        stopRecordingButton.enabled = false
+        
+        audioRecorder.stop()
+        let sessionAudio = AVAudioSession.sharedInstance()
+        try! sessionAudio.setActive(false)
+    }
+    
+    func audioRecorderDidFinishRecording(recorder: AVAudioRecorder, successfully flag: Bool) {
+        if (flag) {
+            self.performSegueWithIdentifier(stopRecordingSegue, sender: audioRecorder.url)
+        } else {
+            print("Recording failed")
+        }
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if (segue.identifier == stopRecordingSegue) {
+            let playSoundsVC = segue.destinationViewController as! PlaySoundsViewController
+            let recordedAudioURL = sender as! NSURL
+            playSoundsVC.recordedAudioURL = recordedAudioURL
+        }
+    }
+}
